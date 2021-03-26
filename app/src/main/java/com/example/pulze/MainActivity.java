922e6f6d4b80a1db289bdff857f57f72fc800a2e
Chamath Wanigasooriya <chamathwanigasooriya@gmail.com>
2021-03-26 20:57:06 +0530

    Added Side Drawer and Floating Action Button

--- app/src/main/java/com/example/pulze/MainActivity.java
+++ app/src/main/java/com/example/pulze/MainActivity.java
@@ -1,14 +1,72 @@
 package com.example.pulze;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private ExtendedFloatingActionButton floatingActionButton;
+    MaterialToolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        floatingActionButton = findViewById(R.id.extended_fab);
+        toolbar = findViewById(R.id.topAppBar);
+        drawerLayout = findViewById(R.id.drawer);
+        NavigationView navigationView = findViewById(R.id.nav_main);
+        //Set Toolbar as ActionBar
+        setSupportActionBar(toolbar);
+
+        //Setup Navigation Drawer
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.openNavDrawer,
+                R.string.closeNavDrawer
+        );
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        //Pass context to side-nav item click listener.
+        navigationView.setNavigationItemSelectedListener(this);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animation = AnimationUtils.loadAnimation(getApplicationContext(),
+                        R.anim.fab_close);
+                v.startAnimation(animation);
+            }
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        if (item.getItemId() == R.id.item1) {
+            Toast.makeText(getApplicationContext(), "Clicked Item 1", Toast.LENGTH_LONG).show();
+        }
+        //close navigation drawer
+        drawerLayout.closeDrawers();
+        return true;
     }
 }
